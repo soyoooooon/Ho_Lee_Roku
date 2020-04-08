d16345d56bc973818d3a204073ede7b4ad23fe7a
이소윤 <s_lee77176@fanshaweonline.ca>
2020-04-08 18:25:41 -0400

    login with php

---
+++ js/components/mainComponent.js
@@ -0,0 +1,12 @@
+export default{
+template: `<section id="select-bg">
+<div class="watching"><h2>Who's watching</h2>
+<span class="user-edit"><img src="images/user_edit.svg"></span>
+<span class="user-plus"><img src="images/user_plus.svg"></span>
+</div>
+
+
+</section>`
+
+
+};
