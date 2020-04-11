c5fc0b052a28d3683f924aed8cd18b721eec48c6
daffodilho <daffodilho@gmail.com>
2020-04-11 15:24:26 -0400

    making style changes to select user page

--- js/components/getUserComponent.js
+++ js/components/getUserComponent.js
@@ -3,24 +3,25 @@ export default {
     props: ['liveuser'],
 
     template: `
-    <div class="GetUser">
-                <img :src="'images/' + liveuser.avatar" >
-                <p>{{ liveuser.username }}</p>
-        
+    <div class="getUser">
+        <div class="imageWrap" @click.prevent="userSelected()">
+            <img :src="'images/' + liveuser.avatar" >
+        </div>
+        <p>{{ liveuser.username }}</p>
     </div>`,
 
     created: function() {
         if (this.liveuser.avatar === null || this.liveuser.avatar === ""){
-            this.liveuser.avatar = "user_plus.svg";
+            this.liveuser.avatar = "user_icon.svg";
         }
 
     },
 
 
     methods: {
-           
-
-
+        userSelected() {
+            debugger;
+        }
     }
 
 }
